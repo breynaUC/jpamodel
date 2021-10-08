04e1a7b2c0d350b7bd9efcd5892ef54795d71ac8
DReyna <reyna_b@upeu.edu.pe>
2021-10-08 12:57:02 -0500

    crud post

---
+++ src/main/java/com/example/modeloThymeleaf/ModeloThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.modeloThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloThymeleafApplication.class, args);
+	}
+
+}
